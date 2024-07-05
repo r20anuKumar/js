a9bae92b59c9c1c57040c157eb8435481b5c4b6a
r20anuKumar <kranu8840@gmail.com>
2024-07-05 08:27:29 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,28 @@
+ "use strict"; // treat all JS code as newer version
+
+//  alert(5+5) // we are using nodejs, not browser
+
+console.log(5
+    +
+    7);   // code readability should be high
+
+console.log("Ranu");
+
+
+let name = "ranu"
+let age = 19
+let isLoggedIn = false
+
+// number => 2 to power 53
+// bigint
+//  string = ""
+// boolean = false / true
+// null = standalone value 
+// undefined = 
+//  symbol = unique  
+
+
+//object 
+
+console.log(typeof  undefined); // undefined
+console.log(typeof null);   // object
