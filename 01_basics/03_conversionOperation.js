dc9ad6978d750c83c5a8d5ae0b680c6ba00edd5c
r20anuKumar <kranu8840@gmail.com>
2024-07-06 07:53:54 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,27 @@
+// let score =  null
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+// "33 " = 33
+//"33abc" = NaN
+// "true" = 1; false = 0
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1 = ture ; 0 = false
+//"" = false
+// "ranu" = true 
+
+let  someNumber = 55
+let StringNumber = String(someNumber)
+console.log(StringNumber);
+console.log(typeof StringNumber);
+
+
