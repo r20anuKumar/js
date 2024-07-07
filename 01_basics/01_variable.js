6d2f5350a8c9ea9f90d38de6c028e2765050f133
r20anuKumar <kranu8840@gmail.com>
2024-07-07 08:50:59 +0000

    01_variable

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,20 @@
+const  accountId = 3545298
+let  accountEmail = "ranu@9gmail.com"
+var accountPassword = "58574"
+accountCity = "Lucknow "
+let accountState;
+
+// accountId = 2 // not allowed 
+
+accountEmail = "hdjh@gmail.con"
+accountPassword = "45478q375"
+accountCity = "greaternoida"
+
+console.log(accountEmail);
+
+/*
+prefer not use var
+because of issue in block scope and functional scope 
+*/
+
+console.log([accountId,accountEmail,accountPassword,accountCity,accountState]);
